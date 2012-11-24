f2912ffe4d2e659ea1042b9d8699c0d44d9de918
Eric Pierce <wapcaplet88@gmail.com>
2012-11-24 15:41:00 -0700

    Move loadScript into separate file
    Not sure if I'll be needing this

---
+++ static/js/load_script.js
@@ -0,0 +1,25 @@
+// Load a javascript file from the given url, and call the given callback when
+// it's done loading.
+function loadScript(url, callback){
+  var script = document.createElement("script")
+  script.type = "text/javascript";
+
+  if (script.readyState){  //IE
+    script.onreadystatechange = function(){
+      if (script.readyState == "loaded" ||
+          script.readyState == "complete"){
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  //Others
+    script.onload = function(){
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+
